e255dfd58c150726a31cf10d99fe78f23d15615c
Josimarmn <mnjosimar@gmail.com>
2024-12-12 19:56:34 -0300

    ok

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('categoria', views.categoria, name="categoria"),
     path('form_categoria', views.form_categoria, name="form_categoria"),
+    path('editar_categoria/<int:id>/',views.editar_categoria, name='editar_categoria'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
@@ -14,7 +14,13 @@ def categoria(request):
     return render(request, 'categoria/lista.html',contexto)
 
 def form_categoria(request):
-    form = CategoriaForm()
+    if request.method == 'POST':
+       form = CategoriaForm(request.POST) # instancia o modelo com os dados do form
+       if form.is_valid():# faz a validação do formulário
+            form.save() # salva a instancia do modelo no banco de dados
+            return redirect('categoria') # redireciona para a listagem
+    else:# método é get, novo registro
+        form = CategoriaForm() # formulário vazio
     contexto = {
         'form':form,
     }
@@ -40,3 +46,16 @@ def excluir_produto(request, id):
         'form':form,
     }
     return render(request, 'categoria/formulario.html', contexto)
+
+def editar_categoria(request, id):
+    categoria = Categoria.objects.get(pk=id)
+    if request.method == 'POST':
+        # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            categoria = form.save() # save retorna o objeto salvo
+            return redirect('categoria') # redireciona para a listagem
+    else:
+         form = CategoriaForm(instance=categoria)
+    return render(request, 'categoria/formulario.html', {'form': form,})
+
