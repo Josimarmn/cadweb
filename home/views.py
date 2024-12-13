5d692dfad5103c7d2270150deb7c564f50708838
Josimarmn <mnjosimar@gmail.com>
2024-12-13 14:47:01 -0300

    ok

--- home/views.py
+++ home/views.py
@@ -57,19 +57,11 @@ def editar_categoria(request, id):
             return redirect('categoria') # redireciona para a listagem
     else:
          form = CategoriaForm(instance=categoria)
-    return render(request, 'categoria/editar.html', {'form': form,})
+    return render(request, 'categoria/formulario.html', {'form': form,})
 
 def remover_categoria(request, id):
-    form = CategoriaForm()
-    contexto = {
-        'form':form,
-    }
     return render(request, 'categoria/formulario.html', contexto)
 
 def detalhes_categoria(request, id):
-    form = CategoriaForm()
-    contexto = {
-        'form':form,
-    }
-    return render(request, 'categoria/detalhes.html', contexto)
+    return redirect('categoria/detalhes.html')
 
