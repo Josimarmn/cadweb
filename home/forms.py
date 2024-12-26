87a9f35090673062169f316a1223c4111997f4c3
Josimarmn <mnjosimar@gmail.com>
2024-12-26 13:27:28 -0300

    ok

--- home/forms.py
+++ home/forms.py
@@ -10,3 +10,13 @@ class CategoriaForm(forms.ModelForm):
             'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
             'ordem':forms.NumberInput(attrs={'class': 'inteiro form-control', 'placeholder': ''}),
         }
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nome', 'cpf', 'datanasc']
+        widgets = {
+            'nome':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
+            'cpf':forms.TextInput(attrs={'class': 'cpf form-control', 'placeholder': 'C.P.F'}),
+            'datanasc': forms.DateInput(attrs={'class': 'data form-control', 'placeholder': 'Data de Nascimento'}, format='%d/%m/%Y'),
+        }
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from .models import *
 from .forms import *
 from django.contrib import messages
 from .models import Categoria
+from .models import Cliente
 
 def index(request):
     return render(request,'index.html')
@@ -53,6 +54,7 @@ def editar_categoria(request, id):
 from django.contrib import messages
 from django.shortcuts import redirect
 from .models import Categoria
+from .models import Cliente
 
 def remover_categoria(request, id):
     try:
@@ -84,3 +86,74 @@ def clean_ordem(self):
     return ordem
 
 
+def cliente(request):
+    contexto = {
+        'listacliente': Cliente.objects.all().order_by('id'),
+    }
+    return render(request, 'cliente/listacliente.html',contexto)
+
+def form_cliente(request):
+    if request.method == 'POST':
+       form = ClienteForm(request.POST) # instancia o modelo com os dados do form
+       if form.is_valid():# faz a validação do formulário
+            cliente = form.save() # salva a instancia do modelo no banco de dados
+            messages.success(request, 'Operação realizada com sucesso!')
+            return redirect('cliente') # redireciona para a listagem
+    else:# método é get, novo registro
+        form = ClienteForm() # formulário vazio
+    contexto = {
+        'form':form,
+    }
+    return render(request, 'cliente/formulariocliente.html', contexto)
+
+
+def editar_cliente(request, id):
+    try:
+        cliente = Cliente.objects.get(pk=id)
+    except Cliente.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+        return redirect('cliente')  # Redireciona para a listagem
+
+
+    if request.method == 'POST':
+        # combina os dados do formulário submetido com a instância do objeto existente, permitindo editar seus valores.
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            cliente = form.save() # save retorna o objeto salvo
+            messages.success(request, 'Operação realizada com Sucesso')
+            return redirect('cliente')
+    else:
+        form = ClienteForm(instance=cliente)
+        
+    return render(request, 'cliente/formulariocliente.html', {'form': form,})
+
+
+from django.contrib import messages
+from django.shortcuts import redirect
+from .models import Cliente
+
+def remover_cliente(request, id):
+    try:
+        # Tenta obter a categoria com o id fornecido
+        cliente = Cliente.objects.get(pk=id)
+        cliente.delete()  # Exclui a categoria encontrada
+        messages.success(request, 'Operação realizada com sucesso')  # Exibe mensagem de sucesso
+    except Cliente.DoesNotExist:
+        # Caso o registro não seja encontrado, exibe a mensagem de erro
+        messages.error(request, 'Registro não encontrado')
+    
+    return redirect('cliente')  # Redireciona para a listagem de categorias
+           
+
+def clean_nome(self):
+    nome = self.cleaned_data.get('nome')
+    if len(nome) < 3:
+        raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
+    return nome  
+    
+def clean_cpf(self):
+    cpf = self.cleaned_data.get('cpf')
+    if cpf <= 0:
+        raise forms.ValidationError("O campo ordem deve ser maior que zero.")
+    return cpf
